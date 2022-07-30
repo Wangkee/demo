d858eda189a001a718bf450358c404183614444d
wangke <wangkee2002@gmail.com>
2022-07-30 14:20:55 +0800

    "master"

--- src/main/java/com/example/demo/v2/TestGit.java
+++ src/main/java/com/example/demo/v2/TestGit.java
@@ -3,5 +3,6 @@ package com.example.demo.v2;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("master");
     }
 }
